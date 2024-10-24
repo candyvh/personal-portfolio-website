d79dcb5256baddc1d026b43f3a73835b3bc19365
candyvh <cvalenciah25@gmail.com>
2024-10-25 03:25:54 +0700

    Updates

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => (
-  <div className="flex justify-center m-3 p-2">
-    <div className="inline-flex items-center space-x-4 text-white">
-      <h2 className="text-2xl">Hi, I'm</h2>
-      <h1 className="text-3xl font-bold">Candy</h1>
-    </div>
-  </div>
-);
-
